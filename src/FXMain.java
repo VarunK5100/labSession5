37060fe967b44e5a3f85e03c85929b90f3c05f49
VarunK5100 <kumar.varun1@northeastern.edu>
2023-10-18 22:52:38 -0400

    commit labsession5

---
+++ src/FXMain.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+
+import com.sun.javafx.logging.Logger;
+import com.sun.javafx.logging.PlatformLogger.Level;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author varun
+ */
+public class FXMain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                try{
+                System.out.println("Hello World!");
+                Parent pane= (Parent) FXMLLoader.load(getClass().getResource("fxml.fxml"));
+                primaryStage.getScene().setRoot(pane);
+                }
+                catch(IOException ex){
+                    System.out.println("exception handled");
+                }
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
